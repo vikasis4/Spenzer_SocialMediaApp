479824e9c172ab2feeed86af7377b575c39497bb
Vikas <vikas@Vikass-Mac.local>
2024-01-07 01:32:51 -0800

    Mac support test

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// Updates from MacBookPro
+
 AppRegistry.registerComponent(appName, () => App);
